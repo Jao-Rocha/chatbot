0fcf92cb14de6df4cc826edffc93bf80b1b9bb3b
João Victor Rocha <joao2013rocha28@hotmail.com>
2023-07-17 16:18:43 -0300

    Aplicacao pronta - problema na key

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import { Chat } from '@/components/Chat'
+
+export default function Home() {
+  return (
+    <main>
+      <div className="flex min-h-screen bg-slate-50 items-center justify-center">
+        <Chat />
+      </div>
+    </main>
+  )
+}
